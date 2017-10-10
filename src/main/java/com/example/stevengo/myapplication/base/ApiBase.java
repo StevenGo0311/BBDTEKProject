2cb3afe52856a21c3f8854f3ff139024fdde43ba
StevenGo <1435374903@qq.com>
2017-10-10 16:15:41 +0800

    修改已知问题

--- src/main/java/com/example/stevengo/myapplication/base/ApiBase.java
+++ src/main/java/com/example/stevengo/myapplication/base/ApiBase.java
@@ -13,7 +13,7 @@ import retrofit2.http.Query;
 
 public interface ApiBase {
    /**采用GET注解，字符串的内容为相对路径*/
-    @GET("/search-ajaxsearch-searchall")
+    @GET(UrlConsTable.URL_SAS)
     /**声明从网络读取数据的方法*/
     Call<MusicEntity> getMusic(@Query("kw")String music_kw,@Query("pi")int music_pi,@Query("pz")int music_pz);
 }
